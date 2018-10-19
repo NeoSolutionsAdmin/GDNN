2dc7b174c36c553c51e8256f596dad332caa185e
ap0r <marianopbernacki@gmail.com>
2018-10-19 10:34:35 -0300

    0.8.0 - Manejo de errores, los parámetros de Ixbatch ya no son hard coded.
    Detectar si no hay facturas recuperadas y tirar error
    Comentar el código que se agregó ayer antes de olvidarme que verga hace
    Agregado Icono a la bandeja del sistema
    Levantar parametros ixbatch de config (no hardcoded)
    Agregar mas puertos a los posibles (COM1 a COM9 y USB001 a USB0013)

--- ControladorFiscal/ControladorFiscal/Form1.cs
+++ ControladorFiscal/ControladorFiscal/Form1.cs
@@ -20,7 +20,7 @@ namespace ControladorFiscal
         //Inicializar app
         private void Form1_Load(object sender, EventArgs e)
         {
-            NotifyIcon1.Icon = SystemIcons.Application;
+            NotifyIcon1.Icon = Icon.ExtractAssociatedIcon(System.Reflection.Assembly.GetExecutingAssembly().Location);
             NotifyIcon1.BalloonTipText = "Controlador Fiscal";
             NotifyIcon1.ContextMenuStrip = MenuAplicacion;
             //inicializar timer
@@ -56,9 +56,9 @@ namespace ControladorFiscal
         //Cuando se cumpla el tiempo, buscar e imprimir facturas pendientes
         private void BuscarFacturasTimer_Tick(object sender, EventArgs e) 
         {
-            
+
             //Bloque de comentarios de referencia
-            
+
             //Esto te tira un listado de facturas A de ese usuario o negocio
             //Lo mismo para las B? si
             //Iteras el listado... a la ultima la imprimis... no hace falta ir
@@ -67,34 +67,21 @@ namespace ControladorFiscal
             //son propiedades, no mnetodos, yo necesito un metodo que la marque en la base de datos..
             //para ver si entendi bien
             //1) armar un listado de todas las facturas entre ayer y mañana
-            //2) de esas facturas, buscar las que ! han sido impresas
+            //2) de esas facturas, buscar las que no han sido impresas
             //3) profit?
             //las que no fueron impresas e imprimirlas
-            //Si el ! significa not
-            //jajajajajaja eso...
             //por ahora arma la base para que envie la ultima factura de ese listad
-            //oka
-            //y como saco de esa factura los items?
-            //MisFacturas[MisFacturas.Count-1].GetDetalle()[0].PRODUCTO.Descripcion etc etc etc //esto te tira un list de DetalleFactura, te diste cuenta?
-            // y como saco si la factura tiene mas de un item? hay un indice?
-            //List<Data2.Class.Struct_DetalleFactura> ElDetalle;
-            //for (int a = 0; a < ElDetalle.Count; a++)
-            //{
-            //    lineatexto =  ElDetalle[a].PRODUCTO.Descripcion; ???
-            //        //vas construyendo la linea de comando en base a las propiedades del detalle
-            //ElDetalle[a].getPrecioFinalSinIva
-            //ElDetalle[a].getTotalSinIva
-            //concatenas todo 
-            //ya
-            //otra cosa tenes datos de ejemplo en la base de datos para que lo pruebe?
-            //que id de usuario necesito?
-            //abri sql
-            //MessageBox.Show("Buscar Facturas");
 
             //Código
 
             //Declarar e inicializar variables
+
+            //variables de configuracion
             int id_usuario = Properties.Settings.Default.Id_Usuario;
+            string puerto = Properties.Settings.Default.Puerto;
+            string velocidad_puerto = Properties.Settings.Default.Velocidad.ToString();
+            
+            //variables varias
             string lineatexto;
             string ContenidoTxtIxbatch;
             string AbreFacturaIxbatch;
@@ -102,22 +89,35 @@ namespace ControladorFiscal
             string NombreCliente ="";
             string CUITCliente = "";
             string DireccionCliente = "";
-
+            
+            //listas
             List<string> ItemsFacturaIxbatch = new List<string>();
             List<string> PrecioItemsFacturaIxbatch = new List<string>();
 
-            List<Data2.Class.Struct_Factura> FacturasRecuperadas = Data2.Class.Struct_Factura.GetFacturasBetweenDates(DateTime.Now.AddDays(-100), DateTime.Now.AddDays(100), id_usuario, false, Data2.Class.Struct_Factura.TipoDeFactura.FacturaB);
-            
-            //TODO Chequear por Null (sin registros)
-            //En caso que FacturasRecuperadas sea null, significa que no hay registros... tira error aca
-            List<Data2.Class.Struct_DetalleFactura> DetalleDeFactura = /*aca*/FacturasRecuperadas[FacturasRecuperadas.Count - 1].GetDetalle();
+            // TODO: el intervalo esta seteado entre +100/-100, fijarse de ponerlo cada 1 dia antes del release.
+
 
             //1) armar listado de facturas entre ayer y mañana
+            List<Data2.Class.Struct_Factura> FacturasRecuperadas = Data2.Class.Struct_Factura.GetFacturasBetweenDates(DateTime.Now.AddDays(-100), DateTime.Now.AddDays(100), id_usuario, false, Data2.Class.Struct_Factura.TipoDeFactura.FacturaB);
+            List<Data2.Class.Struct_DetalleFactura> DetalleDeFactura;
+            
+            //En caso que FacturasRecuperadas sea null, significa que no hay registros... tira error aca
+            try
+            {
+                DetalleDeFactura = FacturasRecuperadas[FacturasRecuperadas.Count - 1].GetDetalle();
+            }
+            catch (Exception error)
+            {
+
+                //esto catchea errores, por lo general si salta se debe a que no hay facturas pendientes de impresion en el periodo
+                MessageBox.Show(error.ToString());
+                return;
+            }
+
             //de ese listado, recuperar la ultima factura y poner sus detalles en una lista
 
             //TODO:           
             //2) comprobar cuales de esas no han sido impresas (pendiente funcionalidad de db a implementar mañana o el lunes o cuando se pueda)
-
             //3) POR AHORA imprimir la última
 
             //4) Parsear esa factura recorriendo item por item y armar un string gigante con comandos de ixbatch
@@ -127,9 +127,7 @@ namespace ControladorFiscal
             {
             if (DetalleDeFactura[a].PRODUCTO != null)
             {
-
                     //TODO: Limitar Descripcion y Domicilio a 25 caracteres
-
                     ItemsFacturaIxbatch.Add(DetalleDeFactura[a].PRODUCTO.Descripcion);
                     PrecioItemsFacturaIxbatch.Add(DetalleDeFactura[a].PRODUCTO.PrecioFinal.ToString());
                     NombreCliente = FacturasRecuperadas[FacturasRecuperadas.Count - 1].senores;
@@ -139,9 +137,9 @@ namespace ControladorFiscal
 
             
 
-            //armar encabezado proba sacando los espacios
+            //armar encabezado
 
-            AbreFacturaIxbatch = "@FACTABRE|T|C|B|1|P|10|E|I|" +NombreCliente+ "||CUIT|"+ CUITCliente + "|N|" + DireccionCliente+ "||| C";
+            AbreFacturaIxbatch = "@FACTABRE|T|C|B|1|P|10|E|I|" +NombreCliente+ "||CUIT|"+ CUITCliente + "|N|" + DireccionCliente+ "|||C";
             //agregar encabezado a factura
             ContenidoTxtIxbatch = AbreFacturaIxbatch;
 
@@ -149,8 +147,6 @@ namespace ControladorFiscal
                 //Recorrer los items
                 for (int i = 0; i < ItemsFacturaIxbatch.Count; i++)
                 {
-                    //TODO limitar a 20 caracteres por item
-
                     //armar una linea usando descripcion de items, precio, etc
                     lineatexto = "@FACTITEM|" + ItemsFacturaIxbatch[i] + "|1|" + PrecioItemsFacturaIxbatch[i] + "|21.00|M|1|0||||0|0";
                     //agregar esa linea al string que luego se guarda como txt y se pasa a ixbatch
@@ -162,31 +158,26 @@ namespace ControladorFiscal
              //agregar cierre de factura
              ContenidoTxtIxbatch = ContenidoTxtIxbatch + Environment.NewLine + CierraFacturaIxbatch;
 
-
             //5) guardar el string gigante contenidotxtixbatch en un txt
-
-            //guardar la factura de ejemplo
             System.IO.File.WriteAllText(Application.StartupPath + "\\ixbatch.txt", ContenidoTxtIxbatch);
 
-            
             //6) llamar a ixbatch y pasarle el txt
-
-            //MessageBox.Show("Mandando a IxBatch");
-
             try
             {
                 System.Diagnostics.Process pProcess = new System.Diagnostics.Process();
                 pProcess.StartInfo.FileName = Application.StartupPath + "\\ixbatchw.exe";
 
-               //TODO hacer que los argumentos salgan de la configuracion en vez de ser hard codeados
-                pProcess.StartInfo.Arguments = "-p COM7 -i ixbatch.txt -o salida.txt -s 9600 "; //argument
+                    //TODO hacer que los argumentos salgan de la configuracion en vez de ser hard codeados
+
+                    pProcess.StartInfo.Arguments = "-p " + puerto + " -i ixbatch.txt -o salida.txt -s " + velocidad_puerto ;
+                    //pProcess.StartInfo.Arguments = "-p i "+ puerto +" -i ixbatch.txt -o salida.txt -s "+velocidad_puerto+" ";
 
                 pProcess.StartInfo.UseShellExecute = false;
                 pProcess.StartInfo.RedirectStandardOutput = true;
                 pProcess.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                pProcess.StartInfo.CreateNoWindow = true; //not diplay a windows
+                pProcess.StartInfo.CreateNoWindow = true;
                 pProcess.Start();
-                string output = pProcess.StandardOutput.ReadToEnd(); //The output result
+                string output = pProcess.StandardOutput.ReadToEnd();
                 pProcess.WaitForExit();
             }
             catch (Exception E)
@@ -195,16 +186,8 @@ namespace ControladorFiscal
             }
 
             //7) resetear la bandera de facturas pendientes
-
-
+            
+            }
         }
-
-
-
-
-    }
-
     }
 }
-
-
