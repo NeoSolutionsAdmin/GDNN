42cacb0ca1c94de3ccc805745f70e6621a5eaefb
David Chaves <dlchaves81@gmail.com>
2018-11-22 00:42:28 -0300

    [Data2] Cambios

--- Suppliers/Data/Class/Struct_DetalleFactura.cs
+++ Suppliers/Data/Class/Struct_DetalleFactura.cs
@@ -74,7 +74,10 @@ namespace Data2.Class
                 else
                 {
                     TRATAMIENTO = new Struct_Treatment();
-                    TRATAMIENTO.Descripcion = _DR["Nombre"].ToString();
+                    TRATAMIENTO.Nombre = _DR["Nombre"].ToString();
+                    TRATAMIENTO.Descripcion = _DR["Descripcion"].ToString();
+                    TRATAMIENTO.Id = int.Parse(_DR["Id"].ToString());
+                    
                     //pendiente de finalizar la carga del tratamiento desde el detalle de factura y el tratamiento en si... Cargar precios coneglados (Deivit)
                 }
             }
