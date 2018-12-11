b8241207917972dc1820a89d20843e2d04d2f6b0
LGN501 <suprapixel21@gmail.com>
2018-12-11 03:25:50 -0300

    [ArqueoCaja] Terminado front end y parte del back end
    Queda hacer la capa de conexión y de negocios.

--- ArqueoCaja/ArqueoCaja/View.ascx.designer.cs
+++ ArqueoCaja/ArqueoCaja/View.ascx.designer.cs
@@ -11,5 +11,14 @@ namespace Christoc.Modules.ArqueoCaja {
     
     
     public partial class View {
+        
+        /// <summary>
+        /// Control url.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField url;
     }
 }
