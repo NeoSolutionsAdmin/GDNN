5eefbfb815c9e63e0cf089ec4f81e2c72919f4d5
David Chaves <dlchaves81@gmail.com>
2018-12-12 02:33:14 -0300

    [Data2] Arqueo de Caja

---
+++ Suppliers/Data/Connection/D_ArqueoDeCaja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Data2.Connection
+{
+    public static class D_ArqueoDeCaja
+    {
+
+        public static void InsertArqueo(int IdLocal, decimal Total, DateTime Fecha, String Anotaciones)
+        {
+            
+        }
+    }
+}
